97c00c0fc7abb59df5995f640c808d93388c99e2
dibyendumajumdar <mobile@majumdar.org.uk>
2022-06-05 19:07:13 +0100

    keywords and names

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -23,40 +23,94 @@ const TOK_IF: i32 = 267;
 const TOK_IN: i32 = 268;
 const TOK_LOCAL: i32 = 269;
 const TOK_DEFER: i32 = 270;
-const TOK_NIL: i32 = 271;
-const TOK_NOT: i32 = 272;
-const TOK_OR: i32 = 273;
-const TOK_REPEAT: i32 = 274;
-const TOK_RETURN: i32 = 275;
-const TOK_THEN: i32 = 276;
-const TOK_TRUE: i32 = 277;
-const TOK_UNTIL: i32 = 278;
-const TOK_WHILE: i32 = 279;
-const TOK_IDIV: i32 = 280;
-const TOK_CONCAT: i32 = 281;
-const TOK_DOTS: i32 = 282;
-const TOK_EQ: i32 = 283;
-const TOK_GE: i32 = 284;
-const TOK_LE: i32 = 285;
-const TOK_NE: i32 = 286;
-const TOK_SHL: i32 = 287;
-const TOK_SHR: i32 = 288;
-const TOK_DBCOLON: i32 = 289;
-const TOK_TO_INTEGER: i32 = 290;
-const TOK_TO_NUMBER: i32 = 291;
-const TOK_TO_INTARRAY: i32 = 292;
-const TOK_TO_NUMARRAY: i32 = 293;
-const TOK_TO_TABLE: i32 = 294;
-const TOK_TO_STRING: i32 = 295;
-const TOK_TO_CLOSURE: i32 = 296;
-const TOK_EOS: i32 = 297;
-const TOK_FLT: i32 = 298;
-const TOK_INT: i32 = 299;
-const TOK_NAME: i32 = 300;
-const TOK_STRING: i32 = 301;
+const TOK_C__DECL: i32 = 271;
+const TOK_C__UNSAFE: i32 = 272;
+const TOK_C__NEW: i32 = 273;
+const TOK_NIL: i32 = 274;
+const TOK_NOT: i32 = 275;
+const TOK_OR: i32 = 276;
+const TOK_REPEAT: i32 = 277;
+const TOK_RETURN: i32 = 278;
+const TOK_THEN: i32 = 279;
+const TOK_TRUE: i32 = 280;
+const TOK_UNTIL: i32 = 281;
+const TOK_WHILE: i32 = 282;
+const TOK_IDIV: i32 = 283;
+const TOK_CONCAT: i32 = 284;
+const TOK_DOTS: i32 = 285;
+const TOK_EQ: i32 = 286;
+const TOK_GE: i32 = 287;
+const TOK_LE: i32 = 288;
+const TOK_NE: i32 = 289;
+const TOK_SHL: i32 = 290;
+const TOK_SHR: i32 = 291;
+const TOK_DBCOLON: i32 = 292;
+const TOK_TO_INTEGER: i32 = 293;
+const TOK_TO_NUMBER: i32 = 294;
+const TOK_TO_INTARRAY: i32 = 295;
+const TOK_TO_NUMARRAY: i32 = 296;
+const TOK_TO_TABLE: i32 = 297;
+const TOK_TO_STRING: i32 = 298;
+const TOK_TO_CLOSURE: i32 = 299;
+const TOK_EOS: i32 = 300;
+const TOK_FLT: i32 = 301;
+const TOK_INT: i32 = 302;
+const TOK_NAME: i32 = 303;
+const TOK_STRING: i32 = 304;
 
 const FIRST_RESERVED: i32 = TOK_OFS + 1;
 const LAST_RESERVED: i32 = TOK_WHILE - TOK_OFS;
+
+static LUA_X_TOKENS: [&str; 48] = [
+    "and",
+    "break",
+    "do",
+    "else",
+    "elseif",
+    "end",
+    "false",
+    "for",
+    "function",
+    "goto",
+    "if",
+    "in",
+    "local",
+    "defer",
+    "C__decl",
+    "C__unsafe",
+    "C__new",
+    "nil",
+    "not",
+    "or",
+    "repeat",
+    "return",
+    "then",
+    "true",
+    "until",
+    "while",
+    "//",
+    "..",
+    "...",
+    "==",
+    ">=",
+    "<=",
+    "~=",
+    "<<",
+    ">>",
+    "::",
+    "@integer",
+    "@number",
+    "@integer[]",
+    "@number[]",
+    "@table",
+    "@string",
+    "@closure",
+    "<eof>",
+    "<number>",
+    "<integer>",
+    "<name>",
+    "<string>",
+];
 
 const CHAR_RET: i32 = '\r' as i32;
 const CHAR_NL: i32 = '\n' as i32;
@@ -79,81 +133,23 @@ const CHAR_FSLASH: i32 = '/' as i32;
 const CHAR_TILDE: i32 = '~' as i32;
 const CHAR_COLON: i32 = ':' as i32;
 
-const luai_ctype_: [i32; 257] = [
-    0x00, /* EOZ */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* 0. */
-    0x00, 0x08, 0x08, 0x08, 0x08, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* 1. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04,
-    0x04, /* 2. */
-    0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x16, 0x16, 0x16, 0x16, 0x16, 0x16, 0x16,
-    0x16, /* 3. */
-    0x16, 0x16, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15,
-    0x05, /* 4. */
-    0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05,
-    0x05, /* 5. */
-    0x05, 0x05, 0x05, 0x04, 0x04, 0x04, 0x04, 0x05, 0x04, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15,
-    0x05, /* 6. */
-    0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05,
-    0x05, /* 7. */
-    0x05, 0x05, 0x05, 0x04, 0x04, 0x04, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* 8. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* 9. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* a. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* b. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* c. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* d. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* e. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, /* f. */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-];
-
-const ALPHABIT: i32 = 0;
-const DIGITBIT: i32 = 1;
-const PRINTBIT: i32 = 2;
-const SPACEBIT: i32 = 3;
-const XDIGITBIT: i32 = 4;
-
-fn MASK(B: i32) -> i32 {
-    1 << B
-}
-
-fn testprop(c: i32, p: i32) -> i32 {
-    luai_ctype_[c as usize + 1] & p
-}
-
-fn lislalpha(c: i32) -> bool {
-    testprop(c, MASK(ALPHABIT)) != 0
-}
-
-fn lislalnum(c: i32) -> bool {
-    testprop(c, MASK(ALPHABIT) | MASK(DIGITBIT)) != 0
-}
-
-fn lisdigit(c: i32) -> bool {
-    testprop(c, MASK(DIGITBIT)) != 0
-}
-
-fn lisspace(c: i32) -> bool {
-    testprop(c, MASK(SPACEBIT)) != 0
-}
-
-fn lisxdigit(c: i32) -> bool {
-    testprop(c, MASK(XDIGITBIT)) != 0
+fn is_keyword(s: &String) -> i32 {
+    for i in 0..LUA_X_TOKENS.len() - 1 {
+        let ch = LUA_X_TOKENS[i].bytes().nth(0).unwrap();
+        if ch.is_ascii_alphanumeric() || ch == b'@' {
+            if LUA_X_TOKENS[i] == s {
+                return i as i32;
+            }
+        }
+    }
+    -1
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum SemInfo {
     IntegerLit(lua_Integer),
     NumberLit(lua_Number),
-    StringLit(String),
+    StringLit(String, bool),
 }
 
 impl fmt::Display for SemInfo {
@@ -165,7 +161,7 @@ impl fmt::Display for SemInfo {
             SemInfo::NumberLit(r) => {
                 write!(f, "{}", r)
             }
-            SemInfo::StringLit(s) => {
+            SemInfo::StringLit(s, is_reserved) => {
                 write!(f, "{}", s)
             }
         }
@@ -176,14 +172,17 @@ impl fmt::Display for SemInfo {
 pub struct Token {
     token: i32,
     /* Token value or character value; token values start from FIRST_RESERVED which is 257, values < 256
-                   are characters */
+    are characters */
     seminfo: Option<SemInfo>,
     /* Literal associated with the token, only valid when token is a literal or an identifier, i.e.
-                                 token is > TOK_EOS */
+    token is > TOK_EOS */
 }
 
 fn tok(t: i32) -> Option<Token> {
-    Some(Token { token: t, seminfo: None })
+    Some(Token {
+        token: t,
+        seminfo: None,
+    })
 }
 
 pub struct Lexer {
@@ -298,7 +297,10 @@ impl Lexer {
             let cl = self.buff[range].to_vec();
             Some(Token {
                 token: TOK_STRING,
-                seminfo: Some(StringLit(String::from_utf8(cl).expect("String expected"))),
+                seminfo: Some(StringLit(
+                    String::from_utf8(cl).expect("String expected"),
+                    false,
+                )),
             })
         } else {
             None
@@ -427,7 +429,26 @@ impl Lexer {
 
                 _ => {
                     let b = self.current as u8;
-                    if lislalnum(self.current) {} else {
+                    if b.is_ascii_alphanumeric() {
+                        loop {
+                            self.save_and_next_ch();
+                            let b = self.current as u8;
+                            if !b.is_ascii_alphanumeric() {
+                                break;
+                            }
+                        }
+                        let w = String::from_utf8(self.buff.to_vec()).expect("String expected");
+                        let tokn = is_keyword(&w);
+                        if tokn != -1 {
+                            /* reserved word? */
+                            break tok(tokn + FIRST_RESERVED);
+                        } else {
+                            break Some(Token {
+                                token: TOK_NAME,
+                                seminfo: Some(StringLit(w, false)),
+                            });
+                        }
+                    } else {
                         let c = self.current;
                         self.next_ch();
                         break tok(c);
@@ -452,11 +473,7 @@ impl Lexer {
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        Lexer, StringLit, CHAR_COLON, CHAR_EQUAL, CHAR_FSLASH, CHAR_GT, CHAR_HYPEN, CHAR_LBRACE,
-        CHAR_LT, CHAR_RBRACE, CHAR_TILDE, TOK_DBCOLON, TOK_EQ, TOK_GE, TOK_IDIV, TOK_LE, TOK_NE,
-        TOK_SHL, TOK_SHR, TOK_STRING,
-    };
+    use crate::{Lexer, StringLit, CHAR_COLON, CHAR_EQUAL, CHAR_FSLASH, CHAR_GT, CHAR_HYPEN, CHAR_LBRACE, CHAR_LT, CHAR_RBRACE, CHAR_TILDE, TOK_DBCOLON, TOK_EQ, TOK_GE, TOK_IDIV, TOK_LE, TOK_NE, TOK_SHL, TOK_SHR, TOK_STRING, TOK_NAME};
     use crate::{Source, CHAR_COMMA, CHAR_LPAREN, CHAR_RPAREN, TOK_EOS};
 
     #[test]
@@ -474,7 +491,7 @@ multi line
  ( )
   [[ a string ]]
 = == < <= << > >= >> // / ~ ~= :: :
-        ";
+      x  ";
 
         let mut source = Source::new(source_string);
         let mut lexer = Lexer::new(source);
@@ -493,7 +510,7 @@ multi line
         lexer.next_token();
         assert_eq!(TOK_STRING, lexer.t.as_ref().unwrap().token);
         assert_eq!(
-            StringLit(" a string ".to_string()),
+            StringLit(" a string ".to_string(), false),
             lexer.t.as_ref().unwrap().seminfo.as_ref().unwrap().clone()
         );
         lexer.next_token();
@@ -524,6 +541,12 @@ multi line
         assert_eq!(TOK_DBCOLON, lexer.t.as_ref().unwrap().token);
         lexer.next_token();
         assert_eq!(CHAR_COLON, lexer.t.as_ref().unwrap().token);
+        lexer.next_token();
+        assert_eq!(TOK_NAME, lexer.t.as_ref().unwrap().token);
+        assert_eq!(
+            StringLit("x".to_string(), false),
+            lexer.t.as_ref().unwrap().seminfo.as_ref().unwrap().clone()
+        );
         lexer.next_token();
         assert_eq!(TOK_EOS, lexer.t.as_ref().unwrap().token);
     }
